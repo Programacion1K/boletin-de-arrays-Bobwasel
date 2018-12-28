3e0b608e6fca8de22bce9d93e08f0e4bbbc76f80
Leroy Merlos <leroymerlos@gmail.com>
2018-12-28 12:33:49 +0100

    Modificación del cuadro de diálogo
    Se ha sustituido los JButton por el Object.
    También se ha añadido cuando le das a salir el "¿Desea salir?".

--- src/InterfazIp.java
+++ src/InterfazIp.java
@@ -1,24 +1,30 @@
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 public class InterfazIp {
     public static void main (String[] args){
         Icon icono = new ImageIcon("img/ip.png");
-        JButton btn[] = new JButton[3];
+        Object btn[] = {"Cadena normal", "Cuatro bytes", "Salir"};
+        int noSalir = 1;
 
-        btn[0] = new JButton("Cadena normal");
-        btn[1] = new JButton("Cuatro Bytes");
-        btn[2] = new JButton("Salir");
+        while(noSalir == 1) {
+            int resultado = JOptionPane.showOptionDialog(null, 
+                                "Elija un método para introducir la IP", 
+                                "Introducir IP", 
+                                JOptionPane.YES_NO_CANCEL_OPTION, 
+                                JOptionPane.QUESTION_MESSAGE, 
+                                icono, 
+                                btn, 
+                                btn[2]);
+            if(resultado == 0){
 
-        JOptionPane.showOptionDialog(null, 
-            "Elija un método para introducir la IP", 
-            "Introducir IP", 
-            JOptionPane.YES_NO_CANCEL_OPTION, 
-            JOptionPane.QUESTION_MESSAGE, 
-            icono, 
-            btn, 
-            btn[0]);
+            } else if(resultado == 1){
+
+            }else{
+                noSalir = JOptionPane.showConfirmDialog(null, "¿Desea salir?"
+                            ,"Salir",JOptionPane.YES_NO_OPTION);
+            }
+        }
     }
 }
